7c414179b21ad8bedeb5007c0f53a30967be56da
Mathis Brossier <mathis.brossier@gmail.com>
2022-01-08 12:28:08 +0100

    Duel

--- src/Duel.hpp
+++ src/Duel.hpp
@@ -1,13 +1,41 @@
 #pragma once
 
+#include <vector>
+
+#include "Card.hpp"
+#include "Player.hpp"
 
 class Duel {
+  
 
 public:
-Duel();
-virtual ~Duel();
+  
+Duel(
+  Card const* cardAttacker,
+  std::vector<Card const*> cardBlockers,
+  Player const* playerAttacker
+) : cardAttacker(cardAttacker), cardBlockers(cardBlockers), playerAttacker(playerAttacker), 
+    cardAttackerDamage(0), cardBlockersDamages(cardBlockers.size()), playerBlockerDamage(0)
+{}
+
+  int getCardAttackerDamage() const;
+  std::vector<int> getCardBlockersDamages() const;
+  int getPlayerAttackerDamage() const;
+  int getPlayerBlockerDamage() const;
+
+  void setCardAttackerDamage(int damage);
+  void setCardBlockersDamages(std::vector<int> damage);
+  void setPlayerAttackerDamage(int damage);
+  void setPlayerBlockerDamage(int damage);
 
 private:
+  Card const* cardAttacker;
+  std::vector<Card const*> cardBlockers;
+  Player const* playerAttacker;
+  Player const* playerBlocker;
 
+  int cardAttackerDamage;
+  std::vector<int> cardBlockersDamages;
+  int playerAttackerDamage;
+  int playerBlockerDamage;
 };
-
